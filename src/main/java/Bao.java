100fdda80a67a87feab532322e8af33f7ffcec69
Chanyf123 <chanyifeng123@gmail.com>
2026-01-23 00:32:32 +0800

    Add Bao.java; Give chatbot a new name; Level 0

---
+++ src/main/java/Bao.java
@@ -0,0 +1,12 @@
+public class Bao {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________\n";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Bao\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(horizontalLine);
+    }
+}
