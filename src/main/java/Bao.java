83f263f826ac24fb92ea7991277a9298193fa9de
Chanyf123 <chanyifeng123@gmail.com>
2026-01-27 23:20:35 +0800

    Add echo loop and exit command

--- src/main/java/Bao.java
+++ src/main/java/Bao.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Bao {
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________\n";
+        String horizontalLine = "____________________________________________________________";
 
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Bao\n");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm Bao");
+        System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(horizontalLine);
+
+        String user_input;
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            user_input = in.nextLine();
+            System.out.println(horizontalLine);
+
+            if (user_input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            } else{
+                    System.out.println(" " + user_input);
+                    System.out.println(horizontalLine);
+            }
+        }
     }
 }
