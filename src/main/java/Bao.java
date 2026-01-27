df108b155a76edbb1552df4293d96864cad16377
Chanyf123 <chanyifeng123@gmail.com>
2026-01-27 23:35:43 +0800

    Add user input storage and list command

--- src/main/java/Bao.java
+++ src/main/java/Bao.java
@@ -3,27 +3,36 @@ import java.util.Scanner;
 public class Bao {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
+        String[] list = new String[100];
+        int itemCount = 0;
+
 
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Bao");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
 
-        String user_input;
+        String userInput;
         Scanner in = new Scanner(System.in);
 
         while (true){
-            user_input = in.nextLine();
+            userInput = in.nextLine();
             System.out.println(horizontalLine);
 
-            if (user_input.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(horizontalLine);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                }
             } else{
-                    System.out.println(" " + user_input);
-                    System.out.println(horizontalLine);
+                list[itemCount] = userInput;
+                itemCount++;
+                System.out.println(" added: " + userInput);
             }
+            System.out.println(horizontalLine);
         }
     }
 }
