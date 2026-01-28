1a532cad29ff07a702e0bd5a4a455326c01a530c
Chanyf123 <chanyifeng123@gmail.com>
2026-01-28 10:18:29 +0800

    Add Task.java; Implement mark and unmark task feature

--- src/main/java/Bao.java
+++ src/main/java/Bao.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class Bao {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
-        String[] list = new String[100];
-        int itemCount = 0;
-
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Bao");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
 
-        String userInput;
         Scanner in = new Scanner(System.in);
 
         while (true){
-            userInput = in.nextLine();
+            String userInput = in.nextLine();
             System.out.println(horizontalLine);
 
             if (userInput.equalsIgnoreCase("bye")) {
@@ -24,12 +22,23 @@ public class Bao {
                 System.out.println(horizontalLine);
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
-                for (int i = 0; i < itemCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
-            } else{
-                list[itemCount] = userInput;
-                itemCount++;
+            } else if (userInput.startsWith("mark ")) {
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println(" [" + tasks[index].getStatusIcon() + "] " +  tasks[index].description);
+            }else if (userInput.startsWith("unmark ")) {
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println(" [" + tasks[index].getStatusIcon() + "] " +  tasks[index].description);
+            }else{
+                tasks[taskCount] = new Task(userInput);
+                taskCount++;
                 System.out.println(" added: " + userInput);
             }
             System.out.println(horizontalLine);
