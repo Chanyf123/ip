4b51802c9019f8aa10d87da4070d8b87c316108e
Chanyf123 <chanyifeng123@gmail.com>
2026-02-05 21:55:31 +0800

    Fix spacing before opening braces

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,9 @@
-public class Event extends Task{
+public class Event extends Task {
 
     protected String from;
     protected String to;
 
-    public Event(String description, String start, String end){
+    public Event(String description, String start, String end) {
         super(description);
         this.from = start;
         this.to = end;
