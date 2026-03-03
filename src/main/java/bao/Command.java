68eea9ab1e92733aac6ddfa64485e71e1dd3ec44
Chanyf123 <chanyifeng123@gmail.com>
2026-03-03 21:24:00 +0800

    Remove unneccessary library imports in Command class

--- src/main/java/bao/Command.java
+++ src/main/java/bao/Command.java
@@ -1,12 +1,5 @@
 package bao;
 
-import bao.Ui;
-import bao.Storage;
-import bao.BaoException;
-import bao.task.Task;
-
-import java.util.ArrayList;
-
 public abstract class Command {
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws BaoException;
 
